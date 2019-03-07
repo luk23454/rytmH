75f7255be281b5ca37c7925f7d7874a89e8dcb4c
luk23454 <luk23454@onet.pl>
2019-03-07 20:38:50 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1551987410,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/kafel-sheet0.png",
+		"images/terenplanszy-sheet0.png",
+		"images/kartaikona-sheet0.png",
+		"images/kartaikona-sheet1.png",
+		"images/metronom-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/karta-sheet0.png",
+		"images/kartywtali-sheet0.png",
+		"images/listakart-sheet0.png",
+		"images/miejscekarty-sheet0.png",
+		"images/gracz-sheet0.png",
+		"images/miejsceataku-sheet0.png",
+		"images/symbol-sheet0.png",
+		"images/tiledbackground.png",
+		"images/znacznikklawisz-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"karty.xml",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
